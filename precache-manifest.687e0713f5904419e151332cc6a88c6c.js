b865fb4681d8507257b0fb609320019f8aa9137a
Bogdan Druzynin <bogdan.druzhynin@crunch.is>
2020-02-10 19:16:26 +0200

    Updates

---
+++ precache-manifest.687e0713f5904419e151332cc6a88c6c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8248a90a8587a9a1244eceb20d957722",
+    "url": "/letters-game/index.html"
+  },
+  {
+    "revision": "ab88a2c44e761596d19b",
+    "url": "/letters-game/static/css/main.b685bc23.chunk.css"
+  },
+  {
+    "revision": "69ff784f2e6ea14a811d",
+    "url": "/letters-game/static/js/2.5cf55d8a.chunk.js"
+  },
+  {
+    "revision": "46f63f8626516ab37e48a023f0c6fcd4",
+    "url": "/letters-game/static/js/2.5cf55d8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab88a2c44e761596d19b",
+    "url": "/letters-game/static/js/main.fb701939.chunk.js"
+  },
+  {
+    "revision": "1075ea50a708d5c4922e",
+    "url": "/letters-game/static/js/runtime-main.0f903a1b.js"
+  },
+  {
+    "revision": "987e802d53fe6535bca3465b6fec1a7a",
+    "url": "/letters-game/static/media/zoovu-o.987e802d.svg"
+  },
+  {
+    "revision": "509452d28bc679f17bb2aa688349e96c",
+    "url": "/letters-game/static/media/zoovu-u.509452d2.svg"
+  },
+  {
+    "revision": "f04e5e7ec4ba17efc272641f25e9fa3e",
+    "url": "/letters-game/static/media/zoovu-v.f04e5e7e.svg"
+  },
+  {
+    "revision": "98d275b2a558436f42b522be4a58e71d",
+    "url": "/letters-game/static/media/zoovu-z.98d275b2.svg"
+  }
+]);
